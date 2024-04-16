35cb0a0f3c4e1eb16dad925ac59bd0c9e55edafc
Fernando <ibanez.fernando.2018@gmail.com>
2024-04-16 14:33:43 -0300

    Terminado el 3 y empezando el punto 4

--- punto4.c
+++ punto4.c
@@ -8,6 +8,34 @@ Tareas:
 Cada preventista puede visitar hasta 5 clientes, los cuales por cuestiones operativas solo
 puede pedir hasta 10 productos
 Las estructuras de datos necesarias son las siguientes:
+i) Desarrollar una interfaz por consola donde se solicite al usuario la cantidad de
+clientes.
+ii) Solicitar al usuario la carga de los clientes creados dinámicamente en el paso anterior.
+iii) A medida que se dé de alta cada cliente, Generar aleatoriamente la cantidad de
+productos asociados al cliente y sus características.
+Ej: producto cargado nro. 2
+Producto {
+ProductoID=2
+Cantidad = 1;
+*TipoProducto = “Snack”;
+PrecioUnitario = 100;
+}
+iv) Implemente una función que calcule el costo total de un producto. Esta función debe
+recibir como parámetro el producto y devolver el resultado de calcular la Cantidad por
+el PrecioUnitario.
+v) Mostrar por pantalla todo lo cargado. Incluyendo un total a pagar por cliente
+(sumatoria del costo de todos los productos)
+cadena_destinoPágina 4
+Taller de Lenguajes I – 2024
+Programador Universitario / Licenciatura en Informática / Ingeniería en Informática
+Trabajo Práctico N° 3
+5) Modifique el ejercicio nro 3 para que, en lugar de ingresar un número fijo de nombres, el
+usuario pueda indicar previamente la cantidad de nombres que ingresará a continuación.
+6) ¿Qué es una wiki?¿Para que suele usarse? Investigue, cómo usar la wiki del github. Cargue
+en la wiki de su repositorio todas las respuestas.
+7) En la Wiki de su repositorio: explique con sus propias palabras cuál es la diferencia entre
+memoria dinámica y estática. ¿Cuáles son las características de cada una? ¿En qué casos
+se debe usar cada una?
 */
 #include <stdio.h>
 #include <stdlib.h>

a9ef2406b23ca67cd9046598871cc8d33e5d09eb
Fernando <ibanez.fernando.2018@gmail.com>
2024-04-16 12:28:45 -0300

    Empezando

---
+++ punto2.c
@@ -0,0 +1,14 @@
+/*Una empresa necesita simular la producción de los próximos 5 años para tal fin necesita
+generar una matriz (estática) de 5x12 donde cada fila corresponde a un año y cada columna
+es un mes. Ud. debe realizar las siguientes tareas:
+a. Cargue dicha matriz con valores aleatorios entre 10000 y 50000.
+b. Muestre por pantalla los valores cargados
+c. Saque el promedio de ganancia por año y muestrelos por pantalla
+d. Obtenga el valor máximo y el valor mínimo obtenido informando el “año” y el “mes” de
+cuándo ocurrió.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+}
